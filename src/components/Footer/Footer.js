73dc38b69210126927cda835e318fc3d36c942cd
md-hasan-ali <hasanaliringku@gmail.com>
2022-02-09 16:15:07 +0600

    some changes and modify

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './footer.css'
 const Footer = () => {
     return (
         <div className='text-center footer'>
-            <h5 className='m-0'>Copy right &copy; 2021 || Md Hasan Ali</h5>
+            <h5 className='m-0'>Copy right &copy; 2022 || <span style={{ color: '#FF5733' }}>Md Hasan Ali</span></h5>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
     return (
-        <div className='header-area'>
+        <div className='header-area' id='home'>
             <div className="container">
                 <div className="row align-items-center">
                     <div className="col-md-2">
-                        <div className="logo">
+                        <div className="logo" id='home'>
                             <h2>Hasan</h2>
                         </div>
                     </div>
                     <div className="col-md-10">
                         <div className="menu">
-                            <ul className='me-auto'>
-                                <li><a href="/">Home</a></li>
-                                <li><a href="/">About Us</a></li>
-                                <li><a href="/">Projects</a></li>
-                                <li><a href="/">Contact Us</a></li>
+                            <ul>
+                                <li><Link to='/'>Home</Link></li>
+                                <li><a href='#about'>About Us</a></li>
+                                <li><a href='#projects'>Projects</a></li>
+                                <li><a href='#contact'>Contact Us</a></li>
                             </ul>
                         </div>
                     </div>
